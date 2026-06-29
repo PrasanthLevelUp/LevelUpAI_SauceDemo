88f9840494fd74ee81fda1d281be2c8c6414db7a
Prasanth_LevelUp <49033529+PrasanthLevelUp@users.noreply.github.com>
2026-06-29 13:23:59 +0530

    Update LoginPage.ts

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -5,7 +5,7 @@ export class LoginPage extends BasePage {
 
   // 🤖 LevelUp AI Auto-Heal: rule_based (96% confidence)
   username = this.page.locator('page.locator('[data-test="username"]')');
-  password = this.page.locator('#password');
+  password = this.page.locator('password');
   loginBtn = this.page.locator('#login-button');
 
   async login(user: string, pass: string) {
