305b1e26eb757ae4a7059ebe9d6f361090b01665
Prasanth_LevelUp <49033529+PrasanthLevelUp@users.noreply.github.com>
2026-06-30 15:43:24 +0530

    Update LoginPage.ts

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -3,9 +3,7 @@ import { BasePage } from './BasePage';
 
 export class LoginPage extends BasePage {
 
-  // 🤖 LevelUp AI Auto-Heal: rule_based (96% confidence)
   username = this.page.locator('[data-test="username"]');
-  // 🤖 LevelUp AI Auto-Heal: rule_based (96% confidence)
   password = this.page.locator('[data-test="password"]');
   loginBtn = this.page.locator('#login-button');
 
